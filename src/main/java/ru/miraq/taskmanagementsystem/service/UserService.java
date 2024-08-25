4d06bd624473214769bcd08f5a2cf16ed1946782
daniil_iagodkin <tehnokot2008@gmail.com>
2024-08-25 16:40:26 +1000

    getting users by sort was added

--- src/main/java/ru/miraq/taskmanagementsystem/service/UserService.java
+++ src/main/java/ru/miraq/taskmanagementsystem/service/UserService.java
@@ -1,6 +1,9 @@
 package ru.miraq.taskmanagementsystem.service;
 
+import ru.miraq.taskmanagementsystem.dto.user.GetUserDTO;
 import ru.miraq.taskmanagementsystem.dto.user.UserDTO;
+
+import java.util.List;
 
 
 public interface UserService {
@@ -8,5 +11,7 @@ public interface UserService {
     UserDTO getUserByEmail(String email);
 
     boolean existByEmail(String email);
+
+    List<GetUserDTO> getUsersBySortType(String sortType);
 
 }
--- src/main/java/ru/miraq/taskmanagementsystem/service/UserServiceImpl.java
+++ src/main/java/ru/miraq/taskmanagementsystem/service/UserServiceImpl.java
@@ -4,9 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import ru.miraq.taskmanagementsystem.dto.user.GetUserDTO;
 import ru.miraq.taskmanagementsystem.dto.user.UserDTO;
+import ru.miraq.taskmanagementsystem.entity.user.RoleType;
+import ru.miraq.taskmanagementsystem.exception.InputParamException;
 import ru.miraq.taskmanagementsystem.mapper.UserMapper;
 import ru.miraq.taskmanagementsystem.repository.UserRepository;
+
+import java.util.List;
 
 
 @Slf4j
@@ -32,6 +37,37 @@ public class UserServiceImpl implements UserService {
     @Override
     public boolean existByEmail(String email) {
         return userRepository.existsByEmail(email);
+    }
+
+    @Override
+    public List<GetUserDTO> getUsersBySortType(String sortType) {
+        return switch (sortType) {
+            case "0" -> userRepository.findAll()
+                    .stream()
+                    .map(user -> GetUserDTO.builder()
+                            .role(user.getRole())
+                            .email(user.getEmail())
+                            .build())
+                    .toList();
+            case "1" -> userRepository.findAll()
+                    .stream()
+                    .filter(user -> user.getRole().contains(RoleType.ROLE_CUSTOMER))
+                    .map(user -> GetUserDTO.builder()
+                            .role(user.getRole())
+                            .email(user.getEmail())
+                            .build())
+                    .toList();
+            case "2" -> userRepository.findAll()
+                    .stream()
+                    .filter(user -> user.getRole().contains(RoleType.ROLE_EXECUTOR))
+                    .map(user -> GetUserDTO.builder()
+                            .role(user.getRole())
+                            .email(user.getEmail())
+                            .build())
+                    .toList();
+            default -> throw new InputParamException("Неверный тип сортировки");
+        };
+
     }
 
 }
